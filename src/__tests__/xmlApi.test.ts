73426fbaf22e23b5d83c48639651d9c0efd9b197
Jens Zech <J.Zech@Thalia.de>
2020-06-30 00:02:38 +0200

    more tests for xmlapi

--- src/__tests__/xmlApi.test.ts
+++ src/__tests__/xmlApi.test.ts
@@ -1,26 +1,37 @@
-import { XmlApi } from '../xmlApi';
-import { XmlRequest } from '../xmlRequest';
+import {Device, XmlApi} from '../xmlApi';
+import { getVersionObj, getStateObj } from './testData';
 
+const xmlApi = new XmlApi('1.1.1.1', 80);
+const get = jest.spyOn(xmlApi, 'get');
 beforeEach(() => {
+  get.mockReset();
 });
 
 test('XmlApi Constructor', () => {
-  const xmlApi = new XmlRequest('1.1.1.1', 80);
+  const xmlApi = new XmlApi('1.1.1.1', 80);
   expect(xmlApi.host).toBe('1.1.1.1');
   expect(xmlApi.port).toBe(80);
 });
 
+test('XmlApi.getState - Success', done => {
+  get.mockImplementationOnce(() => Promise.resolve(getStateObj));
+  xmlApi.getState('1481').then((deviceList) => {
+    expect(deviceList).not.toBe(null);
+    if (deviceList === null) return;
+    expect(deviceList.length).toBe(1);
+    expect(deviceList[0].iseId).toBe('1481');
+    expect(deviceList[0].channel.size).toBe(2);
+    expect(deviceList[0].channel?.get('1482')?.dataPoint.size).toBe(4);
+    expect(deviceList[0].channel?.get('1501')?.dataPoint.size).toBe(2);
+    expect(JSON.stringify(deviceList[0])).toBe(JSON.stringify(new Device(getStateObj.state.device)));
+    done();
+  });
+});
 
-test('XmlApi.get - Success', async () => {
-  const xmlApi = new XmlApi('1.1.1.1', 80);
-
-  function versionCallback(version: number) {
-    console.log('XML Addon version: ', version);
-  }
-  const dataJson = {
-    _declaration: { _attributes: { version: '1.0', encoding: 'ISO-8859-1' } },
-    version: { _text: '1.20' },
-  };
-
-  return xmlApi.getVersion(versionCallback);
+test('XmlApi.getVersion - Success', done  => {
+  get.mockImplementationOnce(() => Promise.resolve(getVersionObj));
+  xmlApi.getVersion().then((version) => {
+    expect(version).toBe(1.25);
+    done();
+  });
 });
